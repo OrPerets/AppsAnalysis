290f98c84f67c1ba3f387bff6c5baf61760ee8ea
Lotan Hadad <lotan3600@gmail.com>
2022-08-18 18:34:02 +0300

    Build fetch function and added a route

---
+++ server/fetch.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-User-Agent': 'desktop',
+		'X-Proxy-Location': 'US',
+		'X-RapidAPI-Key': 'efded72d34mshf6bf2c499d188d7p1fd487jsnf7fa12714b48',
+		'X-RapidAPI-Host': 'google-search3.p.rapidapi.com'
+	}
+};
+
+const getData = async (url) => {
+    try {
+        const data = await fetch(url,options)
+        return data.json();
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+module.exports = getData
