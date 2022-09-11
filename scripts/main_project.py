93b4da1bebf8dc6dcc7fb0f20d204be4cb359a54
OrPerets <orperets11@gmail.com>
2022-09-11 14:18:03 +0300

    ors updates for main script

--- scripts/main_project.py
+++ scripts/main_project.py
@@ -1,25 +1,23 @@
-from traceback import print_tb
-import numpy as np 
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 from variables import *
-from functions import read_file
-from sklearn.ensemble import RandomForestRegressor
-from sklearn import preprocessing
+import functools as ft
 
 # Reading & Exploring files    
 from apps_exploration import *
-print(apps.head())    
+print(apps.head()) 
+apps_top_features = ['App_Id', 'Price', 'Category'] 
 
 from reviews_exploration import *
 print(reviews.head())
+reviews_top_features = ['App_Id', 'Sentiment','Reviews_Number']
 
 from installs_exploration import *
 print(installs.head())
+installs_top_features = ['App_Id', 'Growth (60 days)', 'Installs']
 
 from rating_exploration import *
 print(rating.head())
+rating_top_features = ['App_Id', '1 Star ratings', '5 Star ratings', 'Thumbs_Up_Count', 'Rating']
 
 # Queries 
 print()
@@ -31,3 +29,14 @@ from reviews_queries import *
 print()
 print("Queries for Apps & Installs charts")
 from queries_app_installs import *
+
+
+dfs = [
+       apps[apps_top_features], 
+       reviews[reviews_top_features], 
+       installs[installs_top_features],
+       rating[rating_top_features]
+       ]
+
+data = ft.reduce(lambda left, right: pd.merge(left, right, on='App_Id'), dfs)
+data.to_csv("learning_data.csv")
